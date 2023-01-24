524fb0bae04467389737d228d922e7c6d60c555d
MNlegion <rkreuser30@gmail.com>
2023-01-23 21:13:01 -0500

    initial node.js setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArgs => {
+    for (let i = 0; i < profileDataAr)
+}
+
+printProfileData(profileDataArgs);
