2ea6a3146703d52f430756a3053aca5ed4b4dd19
MNlegion <rkreuser30@gmail.com>
2023-01-26 19:01:18 -0500

    input capture functionality display in command line

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.splice(2);
 
-const printProfileData = profileDataArgs => {
-    for (let i = 0; i < profileDataAr)
-}
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
 
 printProfileData(profileDataArgs);
