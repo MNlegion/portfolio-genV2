e3f5b6673446d98979ccf5025f4f2fc2c1cce1fb
MNlegion <rkreuser30@gmail.com>
2023-01-28 19:19:19 -0500

    initialized inquirer and npm

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your github user name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
 
 
